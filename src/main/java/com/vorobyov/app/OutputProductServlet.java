bcd73f718cefa3d1cb9d489190ba9aaad5c3959d
Mihail_Vorobyov <ptichkinm@mail.ru>
2021-08-18 23:29:46 +0300

    Final

--- src/main/java/com/vorobyov/app/OutputProductServlet.java
+++ src/main/java/com/vorobyov/app/OutputProductServlet.java
@@ -2,11 +2,14 @@ package com.vorobyov.app;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet (name = "OutputProductServlet", urlPatterns = "/products")
 public class OutputProductServlet extends HttpServlet {
 	private static final Logger logger = LoggerFactory.getLogger(OutputProductServlet.class);
 	
@@ -15,10 +18,8 @@ public class OutputProductServlet extends HttpServlet {
 		logger.info("Log: GET");
 		System.out.println("Output 10 products");
 		
-		String products = fillProducts();
-		
 		resp.setCharacterEncoding("UTF-8");
-		resp.getWriter().printf("<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"></head><body>%s</body></html>", products);
+		resp.getWriter().printf("<head><meta charset=\"utf-8\"></head><body>%s</body>", showProducts());
 	}
 	
 	@Override
@@ -26,19 +27,17 @@ public class OutputProductServlet extends HttpServlet {
 		logger.debug("Log: POST");
 		System.out.println("Output 10 products");
 		
-		String products = fillProducts();
-		
 		resp.setCharacterEncoding("UTF-8");
-		resp.getWriter().printf("<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"></head><body>%s</body></html>", products);
+		resp.getWriter().printf("<head><meta charset=\"utf-8\"></head><body>%s</body>", showProducts());
 	}
 	
-	public String fillProducts () {
+	public String showProducts() {
 		StringBuilder products = new StringBuilder(
-			"<table>" +
+				"<table>" +
 				"<tr><th>id</th><th>Наименование</th><th>Цена</th></tr>");
 		
-		for (int i = 0; i < 10; i++) {
-			Product product = new Product(i, "Product_" + i, (i + 1) * 10.5f);
+		for (int i = 1; i < 11; i++) {
+			Product product = new Product(i, "Product_" + i, i * 10.5f);
 			String str = String.format("<tr><th>%d</th><th>%s</th><th>%.2f</th></tr>", product.id, product.title, product.cost);
 			products.append(str);
 		}
